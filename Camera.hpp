927abf542a9a538478ef5b9cebdf96dad712387e
Dylan Swiggett <dylanswiggett@gmail.com>
2014-06-13 21:31:50 -0700

    A bit more documentation.

--- Camera.hpp
+++ Camera.hpp
@@ -3,13 +3,21 @@
 
 #include "glm/glm.hpp"
 
+// Represents a single point of view in three-dimensional space.
+// Easly convertible to OpenGL matrices.
 class Camera {
  public:
+  // Construct a camera with the given view parameters, but facing in
+  // a default direction from the origin.
   Camera(double FOV, double nearPlane, double farPlane, double aspectRatio);
+  // Construct a camera with the given view parameters, and the given
+  // three direction vectors. These vectors must be orthonormal.
   Camera(glm::vec3 pos, glm::vec3 dir, glm::vec3 up, double FOV,
          double nearPlane, double farPlane, double aspectRatio);
 
+  // Get the view matrix for use in OpenGL.
   const glm::mat4 getCamMatrix() const;
+  // Get the projection matrix for use in OpenGL.
   const glm::mat4 getProjMatrix() const;
  private:
   void build_mat();
@@ -21,5 +29,7 @@ class Camera {
 
 #endif  // _CAMERA_HPP_
 
+// Create a new camera looking from the given position towards
+// the given point.
 Camera *cameraTowards(glm::vec3 pos, glm::vec3 towards, glm::vec3 up,
   double FOV, double nearPlane, double farPlane, double aspectRatio);
--- FramebufferBinder.hpp
+++ FramebufferBinder.hpp
@@ -5,14 +5,24 @@
 #include "GL/gl.h"
 #include "GraphicsPipelineItem.hpp"
 
+// A graphics pipeline element that switches the framebuffer to
+// which subsequent pipeline elements will render.
 class FramebufferBinder : public GraphicsPipelineItem {
  public:
+  // Create a new FramebufferBinder that sets the framebuffer to the
+  // default screenbuffer.
   FramebufferBinder(int w, int h);
+  // Create a new FramebufferBinder. If storeColor or storeDepth is
+  // true, the color buffer and depth buffer (respectively) will be
+  // stored as textures. Otherwise, these will be stored as buffers.
   FramebufferBinder(int w, int h, bool storeColor, bool storeDepth);
 
+  // Returns the id for the color buffer texture, if one is used.
   GLuint get_color_texture();
+  // Returns the id for the depth buffer texture, if one is used.
   GLuint get_depth_texture();
 
+  // See GraphicsPipelineItem.hpp
   void act(ModelInstance **model_instances, int num_instances) override;
  private:
   int w_, h_;
